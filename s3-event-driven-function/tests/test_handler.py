75fe9952d042e40baf4d1dd592935e4e6f81ddda
tecchu11 <agdjmp.ajtt@gmail.com>
2022-03-05 00:38:14 +0900

    Fix test

--- s3-event-driven-function/tests/test_handler.py
+++ s3-event-driven-function/tests/test_handler.py
@@ -5,7 +5,7 @@ from unittest import mock
 from src.handler import parse_event
 
 
-@mock.patch.dict(os.environ, {"HELLO": "HELLO", "WORLD": "WORLD"}, clear=True)
+@mock.patch.dict(os.environ, {"ENV_NAME": "DEV"}, clear=True)
 def test_parse_event():
     event_json = json.load(open('./event.json', 'r'))
     bucket, key = parse_event(event_json)
